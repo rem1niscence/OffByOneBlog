fee5e8f484027cee052675ed72fcdd598d245677
Roniel Valdez <roniel_valdez@outlook.com>
2019-06-19 21:24:07 -0400

    Added dummy view and finished base template

--- django/config/settings.py
+++ django/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#(cgb)!1m4as)rt*(idw!%c0u8ikb-rn4@!ew$r!*lhavsdznu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'common'),
+]
